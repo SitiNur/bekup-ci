b3de09e8f96ea6b9de4c2eea0043bb219ca3ca2d
SitiNur <siti.nurhasanah9292@gmail.com>
2016-10-31 19:56:22 +0700

    crud-ajax

--- application/controllers/Task.php
+++ application/controllers/Task.php
@@ -5,15 +5,107 @@ class Task extends CI_Controller {
 
 	public function __construct(){
 		parent::__construct();
-		$this->load->model('task_model');
+		$this->load->model('task_model','task');
 	}
 	public function index()
 	{
-		$data['task'] = $this->task_model->view();
-		$this->load->view('task_view', $data);
+		/*$data['task'] = $this->task_model->view();
+		$this->load->view('task_view', $data);*/
+		$this->load->helper('url');
+        $this->load->view('task_view');
 	}
+ 
+    public function ajax_list()
+    {
+        $list = $this->task->get_datatables();
+        $data = array();
+        $no = $_POST['start'];
+        foreach ($list as $t) {
+            $no++;
+            $row = array();
+            $row[] = $t->task;
+            $row[] = $t->date;
+            $row[] = $t->time;
 
-	public function simpan(){
+            //add html for action
+            $row[] = '<a class="btn btn-sm btn-primary" href="javascript:void(0)" title="Edit" onclick="edit_task('."'".$t->id."'".')"><i class="glyphicon glyphicon-pencil"></i> Edit</a>
+                  <a class="btn btn-sm btn-danger" href="javascript:void(0)" title="Hapus" onclick="delete_task('."'".$t->id."'".')"><i class="glyphicon glyphicon-trash"></i> Delete</a>';
+         
+            $data[] = $row;
+        }
+ 
+        $output = array(
+                        "draw" => $_POST['draw'],
+                        "recordsTotal" => $this->task->count_all(),
+                        "recordsFiltered" => $this->task->count_filtered(),
+                        "data" => $data,
+                );
+        //output to json format
+        echo json_encode($output);
+    }
+
+    public function ajax_edit($id)
+    {
+        $data = $this->task->get_by_id($id);
+        //$data->dob = ($data->dob == '0000-00-00') ? '' : $data->dob; // if 0000-00-00 set tu empty for datepicker compatibility
+        echo json_encode($data);
+    }
+ 
+    public function ajax_add()
+    {
+        $this->_validate();
+        $data = array(
+                'task' => $this->input->post('task'),
+                'date' => $this->input->post('date'),
+				'time' => $this->input->post('time'), 
+          	);
+        $insert = $this->task->save($data);
+        echo json_encode(array("status" => TRUE));
+    }
+ 
+    public function ajax_update()
+    {
+        $this->_validate();
+       	$data = array(
+                'task' => $this->input->post('task'),
+                'date' => $this->input->post('date'),
+				'time' => $this->input->post('time'), 
+          	);
+        $id=$this->input->post('id');
+        $this->task->update($id, $data);
+
+        echo json_encode(array("status" => TRUE));	
+    }
+ 
+    public function ajax_delete($id)
+    {
+        $this->task->delete_by_id($id);
+        echo json_encode(array("status" => TRUE));
+    }
+ 	
+ 	private function _validate()
+    {
+        $data = array();
+        $data['error_string'] = array();
+        $data['inputerror'] = array();
+        $data['status'] = TRUE;
+ 
+        if($this->input->post('task') == '')
+        {
+            $data['inputerror'][] = 'firstName';
+            $data['error_string'][] = 'First name is required';
+            $data['status'] = FALSE;
+        }
+ 
+        
+        if($data['status'] === FALSE)
+        {
+            echo json_encode($data);
+            exit();
+        }
+    }
+
+	/*public function simpan(){
 		$task = $this->input->post('task');
 		$data = array(
 					'task' => $task,
@@ -22,5 +114,5 @@ class Task extends CI_Controller {
 			);
 		$table = 'tbl_task';
 		$this->task_model->simpan($table,$data);
-	}
+	}*/
 }
--- application/models/Task_model.php
+++ application/models/Task_model.php
@@ -3,15 +3,79 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Task_model extends CI_Model {
 
+	var $table ='tbl_siti';
+	var $order = array('id' => 'desc'); // default order
+
 	public function __construct(){
 		parent::__construct();
+		$this->load->database();
 	}
 
-	public function simpan($table,$data){
+	private function _get_datatables_query()
+    {
+         
+        $this->db->from($this->table);
+ 
+        $order = $this->order;
+        $this->db->order_by(key($order), $order[key($order)]);
+        
+    }
+
+	function get_datatables()
+    {
+        $this->_get_datatables_query();
+        if($_POST['length'] != -1)
+        $this->db->limit($_POST['length'], $_POST['start']);
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    function count_filtered()
+    {
+        $this->_get_datatables_query();
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
+ 
+    public function count_all()
+    {
+        $this->db->from($this->table);
+        return $this->db->count_all_results();
+    }
+ 
+    public function get_by_id($id)
+    {
+        $this->db->from($this->table);
+        $this->db->where('id',$id);
+        $query = $this->db->get();
+ 
+        return $query->row();
+    }
+
+    public function save($data)
+    {
+        $this->db->insert($this->table, $data);
+        return $this->db->insert_id();
+    }
+ 
+    public function update($where, $data)
+    {
+        $this->db->where('id',$where);
+        $this->db->update('tbl_siti', $data);
+        return $this->db->affected_rows();
+    }
+ 
+    public function delete_by_id($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->delete($this->table);
+    }
+
+	/*public function simpan($table,$data){
 		$this->db->insert($table,$data);
 	}
 	public function view(){
 		$query = $this->db->query("SELECT * FROM tbl_task");
 		return $query->result_array();
-	}
+	}*/
 }
